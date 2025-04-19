03648921ddbb3f44e669494db6a796d2291ac2a2
Abreham Kassa <abrehamkassa19@gmail.com>
2025-04-19 17:32:52 +0300

    Add an endpoint to check for websocket connection

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/gorilla/websocket"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -11,8 +14,16 @@ type HealthCheckMessage struct {
 	Message string
 }
 
-func HealthCheck(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	message := HealthCheckMessage {
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	message := HealthCheckMessage{
 		Message: "The API is Working",
 	}
 
@@ -28,14 +39,37 @@ func HealthCheck(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Write(response)
 }
 
+func WebsocketTest(w http.ResponseWriter, r *http.Request) {
+	// Upgrage the HTTP connection to a WebSocket connection
+	conn, err := upgrader.Upgrade(w, r, nil)
 
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	defer conn.Close()
+
+	messageType, p, err := conn.ReadMessage()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Println("Received", string(p))
+
+	if err = conn.WriteMessage(messageType, p); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	router := httprouter.New()
-	router.GET("/health_check", HealthCheck)
+	router.HandlerFunc(http.MethodGet, "/health_check", HealthCheck)
+	router.HandlerFunc(http.MethodGet, "/ws", WebsocketTest)
 
 	server := &http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
 		Handler: router,
 	}
 
