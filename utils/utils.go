fcea9383549bb64200dc7b08a8a32e05b8513f31
Abreham Kassa <abrehamkassa19@gmail.com>
2025-04-20 14:03:48 +0300

    Set up helper method to return json response

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,33 @@
 package utils
 
 import (
+	"encoding/json"
+	"maps"
+	"net/http"
+
 	"github.com/google/uuid"
 )
+
+type envelop map[string]any
 
 func CreateID() string {
 	return uuid.New().String()
 }
+
+func WriteJSON(w http.ResponseWriter, status int, message envelop, headers http.Header) error {
+	res, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	res = append(res, '\n')
+
+	maps.Copy(w.Header(), headers)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(res)
+
+	return nil
+}
+
